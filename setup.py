3605dbb9812134e0ac6e13ae5e847fd13c5c09fb
Markus Holtermann <info@markusholtermann.eu>
2015-05-24 11:43:11 +0200

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='0.6.0',
+    version='0.7.0',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
