448176ffd5d3afd72f683233aac5d66ad9d4db32
Stefan J. Betz <info@stefan-betz.net>
2015-08-03 15:35:50 +0200

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='0.8.1',
+    version='0.8.2',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
