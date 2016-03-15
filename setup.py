d3a9683d5b9513aea1adf7562ba8d91339ef28ac
Stefan J. Betz <info@stefan-betz.net>
2016-03-15 21:37:46 +0100

    Bump version: 0.14.1 → 0.14.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='0.14.1',
+    version='0.14.2',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
