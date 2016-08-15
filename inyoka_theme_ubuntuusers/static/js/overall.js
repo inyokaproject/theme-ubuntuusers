1e993afa3c09152ef84dfc2395fcbe87c791add8
Christoph Volkert <inyoka@christoph-volkert.de>
2016-08-15 17:33:03 +0200

    Matrix overview for forum-permissions
    rendered as table includes a simple filter for forums
    and column-/row-highlighting.

--- inyoka_theme_ubuntuusers/static/js/overall.js
+++ inyoka_theme_ubuntuusers/static/js/overall.js
@@ -474,6 +474,34 @@ $(document).ready(function () {
     }
     document.cookie = "admin_menu=" + menu_status + "; " + exp + "; " + dom + "; path=/";
   });
+
+  // Quick Table Search
+  // https://css-tricks.com/complete-guide-table-element/#article-header-id-25
+  $('#table-search').keyup(function() {
+    var regex = new RegExp($('#table-search').val(), "i");
+    var rows = $('.edit-forum-permissions tbody tr:gt(1)');
+    rows.each(function (index) {
+      title = $(this).children(":first-child").html()
+      if (title.search(regex) != -1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
+  // column highlight inside table
+  // https://css-tricks.com/row-and-column-highlighting/#article-header-id-2
+  $(".edit-forum-permissions table").delegate('td','mouseover mouseleave', function(e) {
+    if (e.type == 'mouseover') {
+      $("colgroup").eq($(this).index()).addClass("hover");
+      $(":first-child th").eq($(this).index()+1).addClass("hover");
+    }
+    else {
+      $("colgroup").eq($(this).index()).removeClass("hover");
+      $(":first-child th").eq($(this).index()+1).removeClass("hover");
+    }
+  });
 });
 
 String.prototype.htmlEscape = function () {
