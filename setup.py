5a127bc2801352064f96be66cc7d7d47e394082e
Christoph Volkert <inyoka@christoph-volkert.de>
2020-08-23 15:55:51 +0200

    Bump version: 0.22.2 → 0.23.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='0.22.2',
+    version='0.23.0',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
