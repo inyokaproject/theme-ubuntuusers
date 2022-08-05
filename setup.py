d545f2894674ec3b5c3cfe895b9ddb47a93978af
Christoph Volkert <git-foo@christoph-volkert.de>
2022-08-05 17:41:26 +0200

    Bump version: 0.26.1 → 0.27.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='0.26.1',
+    version='0.27.0',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
