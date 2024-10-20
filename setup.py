8e15f27a15f8a044754db46b2a00a90984e10d68
Christoph Volkert <inyoka@christoph-volkert.de>
2024-10-20 21:03:38 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='inyoka-theme-ubuntuusers',
-    version='1.0.0',
+    version='1.0.1',
     description='Inyoka ubuntuusers theme',
     long_description=readme,
     author='Inyoka Team',
